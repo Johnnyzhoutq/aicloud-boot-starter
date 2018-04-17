78a2f1a0276ba3e3e910fd6f20a6dd30c903aaaa
Johnnyzhoutq <johnnyzhoutq@gmail.com>
2018-04-17 17:27:48 +0800

    eureka server zone分区

---
+++ aicloud-boot-starter/aicloud-cloud-region-server1/src/main/java/com/aicloud/Application.java
@@ -0,0 +1,21 @@
+package com.aicloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@SpringBootApplication
+@EnableEurekaServer
+public class Application extends SpringBootServletInitializer {
+	/**   
+	* @Title: Application.java  
+	* @Description: TODO(用一句话描述该文件做什么) 
+	* @author ZhouTianQi   
+	* @date 2017年1月2日 下午3:49:01 
+	* @version V1.0   
+	*/
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		
+	}
+}
